84bc9f1034436b860cfa641e66b40f7ebe29a550
KIM JUNKUK <54308434+Kimjunkuk@users.noreply.github.com>
2021-08-29 17:35:07 -0600

    Update If-Else.py

--- Hackerrank/If-Else.py
+++ Hackerrank/If-Else.py
@@ -6,7 +6,32 @@ import random
 import re
 import sys
 
+"""
+Natural Language
+If n is odd, print Weird
+If n is even and in the inclusive range of  to , print Not Weird
+If n is even and in the inclusive range of  to , print Weird
+If n is even and greater than , print Not Weird
 
+Whole range : 1- 100
+
+If n is odd print "Weird"  
+If n even and in range between 2 and 5 print "Not Weird" 
+ 
+Pseudo-code
+if 2 <= n <= 5 / 2 == 0
+print("Not Weird")
+else 
+("Weird")
+
+"""
 
 if __name__ == '__main__':
     n = int(input().strip())
+    
+    if (2<=n<=5)/2==0:
+        print("Not Weird")
+    else :
+        print("Weird")
+    
+
