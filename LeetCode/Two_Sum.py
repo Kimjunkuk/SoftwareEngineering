05dbf46dc3558e45a77abff6ef2889f07ba9fcd2
KIM JUNKUK <54308434+Kimjunkuk@users.noreply.github.com>
2021-08-30 07:40:22 -0600

    Create Two_Sum.py

---
+++ LeetCode/Two_Sum.py
@@ -0,0 +1,11 @@
+"""
+
+array배열 중에서 두개의 수를 더한 값이 target의 값이 되는 배열의 번호를 출력하는 문제
+어떻게 배열의 두수의 합이 target의 값을 가지는 배열의 순번을 찾을 것 인가? 
+0과 1의 합 1과 2의 합 2와 3의 합 비교 
+
+if 
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
