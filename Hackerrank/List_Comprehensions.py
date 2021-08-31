6063a9a7fa1bbc3cdac0b33bf43e879bd541e915
KIM JUNKUK <54308434+Kimjunkuk@users.noreply.github.com>
2021-08-31 17:20:30 -0500

    Update List_Comprehensions.py

--- Hackerrank/List_Comprehensions.py
+++ Hackerrank/List_Comprehensions.py
@@ -67,4 +67,13 @@ This form is similar to the simple form of list comprehension, but it evaluates 
 """
 
 https://docs.python.org/3/tutorial/datastructures.html
+
+>>> combs = []
+>>> for x in [1,2,3]:
+...     for y in [3,1,4]:
+...         if x != y:
+...             combs.append((x, y))
+...
+>>> combs
+[(1, 3), (1, 4), (2, 3), (2, 1), (2, 4), (3, 1), (3, 4)]
 """
