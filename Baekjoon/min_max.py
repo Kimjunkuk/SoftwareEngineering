bf68072d89be0b011b5178005e5cc315973c36a9
KIM JUNKUK <54308434+Kimjunkuk@users.noreply.github.com>
2021-09-03 17:34:09 -0500

    Update min_max.py

--- Baekjoon/min_max.py
+++ Baekjoon/min_max.py
@@ -1,5 +1,8 @@
 
-"""List min and max method use"""
+"""List min and max method use
+Resault: 144968 KB	460 ms
+
+"""
 cnt = int(input())
 numbers = list(map(int, input().split()))
 print(min(numbers), max(numbers))
