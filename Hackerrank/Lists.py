3d299a7f854a151f157c86f59415a96f4ee92bda
KIM JUNKUK <54308434+Kimjunkuk@users.noreply.github.com>
2021-09-06 20:34:18 -0500

    Update Lists.py

--- Hackerrank/Lists.py
+++ Hackerrank/Lists.py
@@ -1,2 +1,35 @@
+"""
+N은 명령어의 수를 나타낸다
+n은 명령어,인덱스 넘버, 값 을 포함한다
+
+주어지는 명령어를 반복적으로 수행한다. 
+N값 반복문의 범위를 설정할때 사용
+n의 값은 배열에 삽입하여 인덱스의 값에 해당하는 기능을 수행할 수 있도록 구현 
+
+식별용 빈 리스트 한개 생성함
+결과물을 담을 리스트 한개 생성함
+입력된 3가지 값을 개별값으로 식별할 수 있도록 해야함 >> n.split()함수 사용
+ex) insert 0 5 => "insert" ,0 ,5
+
+반복적 명령어 식별 
+"insert", "print", "remove", "append", "sort", "pop", "reverse" == 7 commands
+if r[x][0](항상 첫번째 값이 명령어 값을 가짐)=="insert"
+
+        elif l[0]=="remove":
+            resault.remove(l[1])
+        elif l[0]=="append":
+            resault.append(l[1])
+        elif l[0]=="sort":
+            resault.sort()
+        elif l[0]=="pop":
+            resault.pop()
+        elif l[0]=="reverse":
+            resault.reverse
+        elif l[0]=="print":
+            print(resault)
+
+
+"""
+
 if __name__ == '__main__':
     N = int(input())
