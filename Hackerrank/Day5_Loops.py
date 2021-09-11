ee4805607c81886a40ec30a684664d7c8a8bca66
KIM JUNKUK <54308434+Kimjunkuk@users.noreply.github.com>
2021-09-11 09:49:09 -0600

    Create Day5_Loops.py

---
+++ Hackerrank/Day5_Loops.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    for x in range(n):
+        for y in range():
+            n*y = 2
+            print()
