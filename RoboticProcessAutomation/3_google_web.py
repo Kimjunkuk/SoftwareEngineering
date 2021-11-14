e55c82dcd617690dd226f070e9cec0864dc02550
KIM JUNKUK <54308434+Kimjunkuk@users.noreply.github.com>
2021-11-14 17:22:32 -0500

    Create 3_google_web.py

---
+++ RoboticProcessAutomation/3_google_web.py
@@ -0,0 +1,39 @@
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys 
+import time 
+import pyperclip 
+
+def google_download(): 
+    #드라이버 로딩 
+    driver = webdriver.Chrome('./chromedriver.exe') 
+    
+    #사용할 변수 선언 
+    #url = 'https://retaillink.login.wal-mart.com/?resumePath=/as/N3SUi/resume/as/authorization.ping' << 사용불가&로그인 불가 url 
+    #url = 'https://retaillink.login.wal-mart.com/?resumePath=/as/ie6cU/resume/as/authorization.ping' << 사용불가&로그인 불가 url
+    url = 'https://drive.google.com/drive/my-drive'
+    uid = '****' 
+    upw = '****' 
+    
+    driver.get(url) 
+    time.sleep(3)  #로딩 대기
+    
+    # 아이디 입력폼 Rev-1 >> tag_id = driver.find_element_by_id('uname') 
+    # find_element_by_class_name 테그 안에 해당하는 문장을 인식하도록 설정
+    tag_id =driver.find_element_by_id("identifierId")
+    
+    # id 입력 
+    # # 입력폼 클릭 -> paperclip에 선언한 uid 내용 복사 -> 붙여넣기 
+    tag_id.click() 
+    pyperclip.copy(uid) 
+    tag_id.send_keys(Keys.CONTROL, 'v') 
+    time.sleep(3) 
+    
+    #Next 버튼 클릭 
+    login_btn = driver.find_element_by_css_selector("button[jsname='LgbsSe']")
+    login_btn.click() 
+    time.sleep(100) 
+    
+    
+    
+    
+google_download()
