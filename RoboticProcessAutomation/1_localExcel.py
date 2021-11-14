860eba98f50620875701673eb3eca30ad4ea26f7
KIM JUNKUK <54308434+Kimjunkuk@users.noreply.github.com>
2021-11-13 23:34:35 -0500

    Update 1_localExcel.py

--- RoboticProcessAutomation/1_localExcel.py
+++ RoboticProcessAutomation/1_localExcel.py
@@ -1,57 +1,97 @@
-from oauth2client.client import verify_id_token
-from openpyxl import load_workbook # 파일 불러오기
-from bs4 import BeautifulSoup
-from selenium import webdriver
-import time
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys 
+import time 
+import pyperclip 
 
-wb = load_workbook("Walmart PIckups GA Warehouse.xlsx") # Walmart PIckups GA Warehouse.xlsx 파일에서 wb 을 불러옴
-ws = wb.active  # 활성화된 Sheet
-
-"""
-1. Google sheet download (Walmart PIckups GA Warehouse.xlsx) 
-
-2. 빈 Load 번호 cell 정보 확인 
-    2-1. [IF]: 빈칸이 있다면
-        2-1-1. 빈 Load 번호 cell의 좌측 2컬럼 옆 PO 번호를 식별
-        2-1-2. Walmart supplier 웹사이트에 접속
-        2-1-3. ID, PW 정보 입력 로그인 버튼 클릭
-        2-1-4. 상단 Supplier 메뉴 클릭
-        2-1-5. Routing Tools 메뉴 클릭
-        2-1-6. Routing Status 메뉴 클릭
-        2-1-7. Search 메뉴에서 PO# or Load# 메뉴 클릭
-        2-1-8. Search By PO Numbers 메뉴에 Load 번호가 없는 PO 번호 입력
-        2-1-9. 하단의 Search 메뉴 클릭 
-
-        
-    2-2. [IF]: 빈칸이 없다면 
+def walmart_login(): 
+    #드라이버 로딩 
+    driver = webdriver.Chrome('./chromedriver.exe') 
     
-3. 
+    #사용할 변수 선언 
+    #url = 'https://retaillink.login.wal-mart.com/?resumePath=/as/N3SUi/resume/as/authorization.ping' << 사용불가&로그인 불가 url 
+    #url = 'https://retaillink.login.wal-mart.com/?resumePath=/as/ie6cU/resume/as/authorization.ping' << 사용불가&로그인 불가 url
+    url = '****'
+    uid = '****' 
+    upw = '****' 
+    
+    #Walmart 로그인 페이지로 이동 
+    driver.get(url) 
+    time.sleep(3)  #로딩 대기
+    
 
-"""
+    # find_element_by_id
+    # find_element_by_name
+    # find_element_by_xpath
+    # find_element_by_link_text
+    # find_element_by_partial_link_text
+    # find_element_by_tag_name
+    # find_element_by_class_name
 
+    
+    # 아이디 입력폼 Rev-1 >> tag_id = driver.find_element_by_id('uname') 
+    # find_element_by_class_name 테그 안에 해당하는 문장을 인식하도록 설정
+    tag_id =driver.find_element_by_css_selector("input[data-automation-id='uname']")
+    
+    #패스워드 입력폼 Rev-1 >> tag_pw = driver.find_element_by_id('pwd') 
+    tag_pw =driver.find_element_by_css_selector("input[data-automation-id='pwd']")
+    
+    
+    # id 입력 
+    # # 입력폼 클릭 -> paperclip에 선언한 uid 내용 복사 -> 붙여넣기 
+    tag_id.click() 
+    pyperclip.copy(uid) 
+    tag_id.send_keys(Keys.CONTROL, 'v') 
+    time.sleep(1) 
+    
+    # pw 입력 # 입력폼 클릭 -> paperclip에 선언한 upw 내용 복사 -> 붙여넣기 
+    tag_pw.click() 
+    pyperclip.copy(upw) 
+    tag_pw.send_keys(Keys.CONTROL, 'v') 
+    time.sleep(1) 
+    
+    #로그인 버튼 클릭 
+    login_btn = driver.find_element_by_css_selector("button[data-automation-id='loginBtn']")
+    login_btn.click() 
+    time.sleep(6) 
+    
+    # Supplier 버튼 클릭
+    Supplier_btn = driver.find_element_by_id('transom-navbar-Supplier')
+    Supplier_btn.click() 
+    time.sleep(3) 
+    
+    # Routing Tools 버튼 클릭
+    Routing_Tools_btn = driver.find_element_by_id('twist-nav-Routing_Tools')
+    Routing_Tools_btn.click() 
+    time.sleep(2) 
+    
+    # Routing Status 버튼 클릭
+    Routing_Status_btn = driver.find_element_by_link_text("Routing Status").click() # a tag link_text를 직접 클릭할 수 있는 모듈 사용함
+    Routing_Status_btn.click() 
+    time.sleep(2) 
+    
+    # PO# or Load# 버튼 클릭
+    PO_Load_btn = driver.find_element_by_id("mat-tab-label-0-1").click() 
+    PO_Load_btn.click() 
+    time.sleep(100) 
+    
+    
+    #체크 박스 클릭 
+    # login_btn = driver.find_element_by_css_selector("input[type='checkbox']")
+    # login_btn.click() 
+    # time.sleep(1) 
+    
+    
+    
 
-Po_num = []  # Blank가 아닌 PO번호의 셀 정보를 담을 리스트 생성
-Po_num_bl = [] # Blank인 PO번호의 셀 정보를 담을 리스트 생성
-
-
-for x in range(2, 149):
-    y=2
-    verify=ws.cell(row=x, column=y).value #PO 넘버 컬럼의 값을 149low 까지 가져와 변수에 넣는다.
-    if verify == None: #PO#가 null 아닐 경우
-        #PO#의 셀 정보를 리스트에 넣는다. 
-        Po_num.append(ws.cell(row=x, column=y))  #리스트 2번 부터 데이터 삽입 #셀의 정보
- 
-
-    elif verify != "": #PO번호가 null 일경우 
-        Po_num_bl.append(ws.cell(row=x, column=y)) #Po_num_bl 리스트에 PO번호가 null인 셀의 정보를 순차 삽입
-        #print(Po_num_bl) # 모든 Po_num_bl 리스트의 값을 출력 
-
-print(Po_num)
-
-
-
-
-
-
-
-
+    #로그인이 실패했을 경우 - 예: 아이디나 패스워드 불일치 
+    # try: 
+    #     #로그인 실패창 
+    #     login_error = driver.find_element_by_css_selector('#err_common > div > p') 
+    
+    #     print('로그인 실패 > ', login_error.text) 
+    
+    # except: 
+    #     print('로그인 성공') 
+        
+        
+walmart_login()
