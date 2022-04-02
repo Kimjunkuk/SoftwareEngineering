28c232a96a5a1d3452ada26d978c3e381fec0973
Kimjunkuk <ehehwnwjs546@naver.com>
2022-04-01 23:35:02 -0400

    040122_BinarySearch_rev1

--- LeetCode/704. Binary Search.py
+++ LeetCode/704. Binary Search.py
@@ -1,3 +1,4 @@
+# Study
 def search(arr, target):
   row_index = 0
   mid_index = 0
@@ -5,17 +6,42 @@ def search(arr, target):
 
   while high_index >= row_index:
     mid_index = (high_index+row_index)//2
-    
+
     if target > arr[mid_index]:
-      row_index=mid_index+1
-    elif target<arr[mid_index]:
-      high_index=mid_index-1
+      row_index = mid_index+1
+    elif target < arr[mid_index]:
+      high_index = mid_index-1
     else:
       return mid_index
   return -1
+
+
 arr = [1, 2, 3, 4, 5, 6, 7]
 target = 7
 
-final_result=search(arr, target)
+final_result = search(arr, target)
 
 print("The final result:["+str(final_result)+"]")
+
+# result
+# Runtime: 438 ms, faster than 13.98% of Python3 online submissions for Binary Search.
+# Memory Usage: 15.6 MB, less than 24.85% of Python3 online submissions for Binary Search.
+
+# test
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        row_index = 0
+        mid_index = 0
+        high_index = len(nums)-1
+
+        while high_index >= row_index:
+            mid_index = (high_index+row_index)//2
+            if target > nums[mid_index]:
+                row_index = mid_index+1
+            elif target < nums[mid_index]:
+                high_index = mid_index-1
+            else:
+                return mid_index
+
+        return -1
+
