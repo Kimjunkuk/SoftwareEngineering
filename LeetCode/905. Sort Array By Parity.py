f09c23073792e3e0a90e45a9e7c85b69982451d5
Kimjunkuk <ehehwnwjs546@naver.com>
2022-04-03 12:45:21 -0400

    040322_SortArray_rev1

---
+++ LeetCode/905. Sort Array By Parity.py
@@ -0,0 +1,16 @@
+nums = [4, 3, 5, 6, 1, 7, 9, 10]
+
+# for nums in nums:
+#     i=0
+#     j=0
+#     while i <= j:
+#         j = i+1
+#         if nums[j] < nums[i]:
+#             firtsValue=nums[i]
+#             compareValue=nums[j]
+#             nums[i] = compareValue 
+#             nums[j] = firtsValue
+#     i+=1
+j=0
+
+print(type(nums[j]))
