333c8c6425ac18fd8b8ad3e536db5934e9951133
Kimjunkuk <ehehwnwjs546@naver.com>
2022-04-03 17:56:53 -0400

    040322_SortArray_rev2

--- LeetCode/905. Sort Array By Parity.py
+++ LeetCode/905. Sort Array By Parity.py
@@ -1,16 +1,16 @@
-nums = [4, 3, 5, 6, 1, 7, 9, 10]
+arr = [5, 3, 8, 4, 9, 1, 6, 2, 7]
+n = len(arr)
 
-# for nums in nums:
-#     i=0
-#     j=0
-#     while i <= j:
-#         j = i+1
-#         if nums[j] < nums[i]:
-#             firtsValue=nums[i]
-#             compareValue=nums[j]
-#             nums[i] = compareValue 
-#             nums[j] = firtsValue
-#     i+=1
-j=0
+for i in range(0, n-1): #1
+    least = i
+    for j in range (i+1, n):
+        if arr[least] > arr[j]:
+            least = j
+    arr[i], arr[least] = arr[least], arr[i]
+    
+print(arr)
 
-print(type(nums[j]))
+
+#1. 0부터 n-1==8 array 길이의 -1 의 값만큼 반복 수행 << 총 인덱스만큼만 반복 필요 
+#2. least=i 선언
+#3. 이중 반복문에서 상위반복문 의 +1 만큼 진행 
