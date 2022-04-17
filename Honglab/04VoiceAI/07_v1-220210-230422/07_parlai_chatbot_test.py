b4de16cd54659f07994a3e5404df09e8e9a97ac1
Kimjunkuk <ehehwnwjs546@naver.com>
2022-04-16 22:41:33 -0400

    041622_honglab_rev3

---
+++ Honglab/04VoiceAI/07_v1-220210-230422/07_parlai_chatbot_test.py
@@ -0,0 +1,22 @@
+import hlab_parlai_wrapper
+
+# You can choose a model from https://www.parl.ai/docs/zoo.html#model-zoo
+
+# mychatbot = hlab_parlai_wrapper.MyChatbot(model_file = "zoo:wikipedia_full/tfidf_retriever/model") # 위키피디아 암기
+# mychatbot = hlab_parlai_wrapper.MyChatbot(model_file = "zoo:dodecadialogue/empathetic_dialogues_ft/model") # 단순
+# mychatbot = hlab_parlai_wrapper.MyChatbot(
+#     model_file="zoo:blender/blender_400Mdistill/model"  # 3.71G
+# )
+# mychatbot = hlab_parlai_wrapper.MyChatbot(
+#     model_file="zoo:blender/blender_9B/model"  # https://www.parl.ai/docs/zoo.html#blender-9-4b 모델 용량 17.3G
+# )
+
+mychatbot = hlab_parlai_wrapper.MyChatbot(model_file="zoo:blender/blender_90M/model")
+
+while True:
+
+    user_input = input("You: ")
+
+    ai_answer = mychatbot.talk_to_ai(user_input)
+
+    print("AI:", ai_answer)
