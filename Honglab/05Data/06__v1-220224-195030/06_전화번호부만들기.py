d84c7256708efab367ce5d21a73cab4c78ffc0c0
Kimjunkuk <ehehwnwjs546@naver.com>
2022-04-17 20:15:43 -0400

    041722_honglab_rev2

--- Honglab/05Data/06__v1-220224-195030/06_전화번호부만들기.py
+++ Honglab/05Data/06__v1-220224-195030/06_전화번호부만들기.py
@@ -10,24 +10,42 @@
 import os
 import json
 
+from soupsieve import select
+
 
 def main_menu() -> int:
     """사용자의 입력을 받아서 검증하고 적절하다면 정수로 반환합니다."""
-
-    return 5  # 교체
-
+    while user_input:= input("(1)찾기 (2)추가/변경 (3)삭제 (4)모두보기 (5)종료:"):
+        if user_input in ("1","2","3","4","5"):
+            return int(user_input)
+        else:
+            print("잘못된 명령입니다.")
 
 def show_all():
     """모든 데이터 출력"""
 
-    pass
+    if not contact_data:
+        print("현재 등록된 연락처가 없습니다.")
+    else:
+        for k,v in contact_data.items():
+            print("{} {} {}".format(k, v["Phone"], v["Email"]))
 
 
 def find_person():
     """이름을 입력받고 그 이름으로 찾아서 해당 데이터 출력"""
 
-    pass
-
+    user_input = input("이름을 입력해주세요:")
+    
+    if user_input in contact_data:
+        print(
+            "{} {} {}".format(
+                user_input,
+                contact_data[user_input]["Phone"],
+                contact_data[user_input]["Email"],
+            )
+        )
+    else:
+        print(f'"{user_input}"를 찾지 못했습니다.')
 
 def update_person():
     """사람 추가 또는 수정
@@ -36,13 +54,21 @@ def update_person():
     없는 이름이면 새로 추가
     """
 
-    pass
+    name =  input("이름을 입력해주세요:")
+    phone = input("전화번호를 입력해주세요:")
+    email=input("이메일을 입력해주세요:")
 
+    contact_data[name]={"Phone":phone, "Email":email}
 
 def delete_person():
     """이름을 입력받아서 데이터 삭제"""
 
-    pass
+    user_input = input("이름을 입력해 주세요:")
+    
+    if user_input in contact_data:
+        del contact_data[user_input]
+    else:
+        print(f'"{user_input}"를 찾지 못했습니다.')
 
 
 # 프로그램 실행의 시작
@@ -55,15 +81,25 @@ contact_data = {}
 
 if os.path.isfile("my_contacts.json"):
     # 저장 파일이 있을 경우 읽어서 dict 초기화
-    pass
-
+    with open("my_contacts.json", encoding="utf8") as f:
+        contact_data = json.load(f)
+        
 # main_menu()로 받아온 사용자의 입력에 따라 함수 호출
 while (selected := main_menu()) != 5:  # 5는 종료 메뉴
 
-    pass
+    if selected == 1:
+        find_person()
+    elif selected ==2:
+        update_person()
+    elif selected ==3:
+        delete_person()
+    elif selected==4:
+        show_all()
+        
 
 else:
     # 종료하기 전 파일로 데이터 저장
-    pass
+    with open("my_contacts.json", "w", encoding="utf8") as f:
+        json.dump(contact_data, f, indent=2)
 
     print("종료합니다.")
