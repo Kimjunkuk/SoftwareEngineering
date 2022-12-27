6043e81e122172dad65188919a596e229319fce7
Kimjunkuk <ehehwnwjs546@naver.com>
2022-12-27 14:59:20 -0500

    852. Peak Index in a Mountain Array

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -1,9 +1,14 @@
-nums1 = [1,2,3,0,0,0]
-nums2 = [2,5,6]
+arr =[3,4,5,1]
 
-
-m=3
-n=3
-master_nums=nums1[0:m]+nums2[0:n]
-master_nums.sort()
-print(master_nums)
+left_index=0
+center_index=0
+right_index=1
+n=len(arr) - 1
+for x in range(n):
+    if arr[center_index] < arr[right_index+x]:
+        # print(right_index+x)
+        center_index = right_index+x
+    elif arr[center_index] > arr[left_index]:
+        print(center_index)
+    else:
+        print(center_index)
