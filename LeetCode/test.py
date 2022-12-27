8fcedce7be3fbe9ea6bd573a6807c93b896e0ea7
Kimjunkuk <ehehwnwjs546@naver.com>
2022-12-27 13:55:43 -0500

    35. Search Insert Position

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -1,22 +1,9 @@
-nums =[3,2,4]
-target=6
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]
 
-def test():
-    n=len(nums)
-    # print(n)
-    static_value=0
-    compare_value=0
-    master_value=0
-    y=1
-    for x in range(n):
-        static_value = nums[x]
-        # print(x)
-        for y in range(n):
-            compare_value = nums[y]
-            print(y)
-            master_value = static_value + compare_value
-            # print(master_value, static_value, compare_value)
-            if master_value ==  target:
-                return [x,y]
 
-test()
+m=3
+n=3
+master_nums=nums1[0:m]+nums2[0:n]
+master_nums.sort()
+print(master_nums)
