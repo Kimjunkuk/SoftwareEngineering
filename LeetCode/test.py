292ca68ee4f6789709a0c2c333f1f27b47baced5
Kimjunkuk <ehehwnwjs546@naver.com>
2022-12-28 19:06:50 -0500

    350. Intersection of Two Arrays II

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -1,14 +1,10 @@
 arr =[3,4,5,1]
-
+same_value=[]
 left_index=0
 center_index=0
 right_index=1
 n=len(arr) - 1
 for x in range(n):
-    if arr[center_index] < arr[right_index+x]:
-        # print(right_index+x)
-        center_index = right_index+x
-    elif arr[center_index] > arr[left_index]:
-        print(center_index)
-    else:
-        print(center_index)
+    same_value.append(x)
+
+print(same_value)
