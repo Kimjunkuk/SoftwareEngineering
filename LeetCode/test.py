d4ccc1ab6a5eab55526d91d2e70f80ce46c1cd64
Kimjunkuk <ehehwnwjs546@naver.com>
2022-12-30 16:47:00 -0500

    118. Pascal's Triangle

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -1,5 +1,19 @@
-
-
-for x in range(h):
-    for y in range(w):
-        reshape[r][c]=
+numRows=5
+k=1
+temp=[]
+for x in range(numRows):
+    line=[]
+    for y in range(k):
+        line.append(1)
+    k+=1
+    temp.append(line)
+p=1
+for i in range(numRows):
+    n=len(temp[i])
+    for j in range(p):
+        if i > 1 and j >= 1 and j < n-1:
+            l=temp[i-1][j-1]
+            r=temp[i-1][j]
+            temp[i][j]=l+r
+    p+=1
+print(temp)
