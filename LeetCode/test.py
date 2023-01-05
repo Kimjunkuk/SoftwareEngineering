6980e550593ab093c7915b4640fc8bd65ad0007b
Kimjunkuk <ehehwnwjs546@naver.com>
2023-01-04 20:17:40 -0500

    412 Fizz Buzz

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -1,16 +1,14 @@
-s="anagram"
-t="nagaram"
+temp=[1,2,14]
+num=1
+print(isinstance(temp[2]%3, int)
 
-import collections
+# for x in range(3):
+#     temp.append(num+x)
 
-counter= collections.Counter
+# for y in range(3):
+#     if isinstance(temp[y]/3, int) and isinstance(temp[y]/5, int):temp[y]="FizzBuzz"
+#     elif isinstance(temp[y]/3, int):temp[y]="Fizz"
+#     elif isinstance(temp[y]/5, int):temp[y]="Buzz"
+#     else: temp[y]=1+y
 
-sl=counter(list(s))
-tl=counter(list(t))
-
-
-print(sl)
-print(tl)
-
-if sl== tl:
-    print("Good")
+print(temp)
