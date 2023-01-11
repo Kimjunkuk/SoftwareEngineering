4bace82465e2351ab9917fb3fa4168cd8cfd45f4
Kimjunkuk <ehehwnwjs546@naver.com>
2023-01-11 13:21:32 -0500

    1089. Duplicate Zeros

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -1,4 +1,16 @@
-nums=[-4,-1,0,3,10]
-# print(len(nums))
+arry=[1,0,2,3,0,4,5,0]
+zerocounter=0
+n=0
+for x in range(len(arry)):
+    if arry[n] == 0: 
+        arry.insert(n, 0) 
+        n+=1
+        zerocounter+=1
+    n+=1
+arry=arry[:-zerocounter]
 
-print(temp)
+print(arry)
+
+# arry.insert(1, 0)
+# print(arry[:-2])
+# # print(arry)
